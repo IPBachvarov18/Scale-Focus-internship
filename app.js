df0f578d61656b16487ea3f54a868ec3e0432eb6
Ivelin Bachvarov <ipbachvarov18@codingburgas.bg>
2021-07-14 00:13:25 +0300

    Test app.js

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
 const express = require("express");
 const app = express();
+
+app.get("/", function(req,res){
+    res.render("site/index.html");
+})
+
+app.listen(5404,function(req,res){
+    console.log("Start");
+})
+

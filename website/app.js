facb86807d5c30aeac2182c67a199f4f8d40e451
Ivelin Bachvarov <ipbachvarov18@codingburgas.bg>
2021-07-14 14:08:00 +0300

    Implement website routes

--- website/app.js
+++ website/app.js
@@ -1,11 +1,26 @@
-const express = require("express");
-const app = express();
 
-app.get("/", function(req,res){
-    res.render("site/index.html");
-})
+var express = require("express");
+var path = require("path");
 
+var indexRouter = require("./routes/index");
+var loginRouter = require("./routes/login");
+var registerRouter = require("./routes/register");
+
+
+var app = express();
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/", indexRouter);
+app.use("/login", loginRouter);
+app.use("/register", registerRouter);
+
+
+module.exports = app;
 app.listen(5404,function(req,res){
     console.log("Start");
 })
-

b55bce27954479cba5d379e2323e29006e7936fb
Ivelin Bachvarov <ipbachvarov18@codingburgas.bg>
2021-07-14 23:46:43 +0300

    Start linking db with website

--- website/routes/register.js
+++ website/routes/register.js
@@ -18,19 +18,16 @@ router.get("/register", function(req, res) {
 router.post("/register", async function(req, res) {
     try {
         console.log(req.body);
-        //const crypto = require("crypto");
-        //const sha256sum = crypto.createHash("sha256");
-        //const passHashed = sha256sum.update(req.body.password).digest("hex");
+       
         const pool = await sql.connect(config);
         const result = await pool
             .request()
-            .input("username", sql.VarChar, req.body.username)
-            .input("email", sql.VarChar, req.body.email)
-            .input("firstName", sql.VarChar, req.body.firstName)
-            .input("lastName", sql.VarChar, req.body.lastName)
-            .input("passwordHash", sql.NVarChar, req.body.password)
-            .query `INSERT INTO Users (username, email, firstName, lastName, passwordHash)
-            VALUES (@username, @email, @firstName, @lastName, @passwordHash)`;
+            .input("Username", sql.NVarChar, req.body.username)
+            .input("firstName", sql.NVarChar, req.body.firstName)
+            .input("lastName", sql.NVarChar, req.body.lastName)
+            .input("password", sql.NVarChar, req.body.password)
+            .query `INSERT INTO User (username, firstName, lastName, password)
+            VALUES (@username, @firstName, @lastName, @password)`;
         console.log(result);
     } catch (e) {
         console.log(e);
